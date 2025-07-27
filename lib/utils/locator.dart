f573276d39da0441b9ea8121f45c89e38df3c064
Timo <timo.schroeder@os-home.de>
2025-07-27 17:26:35 +0200

    chore: remove unused view model

--- lib/utils/locator.dart
+++ lib/utils/locator.dart
@@ -1,6 +1,5 @@
 import 'package:chessalyst/domain/use_cases/pgn_game_use_case.dart';
 import 'package:chessalyst/ui/analysis_board/view_model/analysis_board_view_model.dart';
-import 'package:chessalyst/ui/game_notation/view_model/game_notation_view_model.dart';
 import 'package:chessalyst/ui/game_viewer/view_model/game_viewer_view_model.dart';
 import 'package:chessalyst/ui/game_viewer/view_model/metadata_form_view_model.dart';
 import 'package:watch_it/watch_it.dart';
@@ -9,9 +8,6 @@ void setupLocator() {
   di.registerLazySingleton<PgnGameUseCase>(() => PgnGameUseCase());
   di.registerLazySingleton<AnalysisBoardViewModel>(
     () => AnalysisBoardViewModel(),
-  );
-  di.registerLazySingleton<GameNotationViewModel>(
-    () => GameNotationViewModel(),
   );
   di.registerLazySingleton<MetadataFormViewModel>(
     () => MetadataFormViewModel(),
